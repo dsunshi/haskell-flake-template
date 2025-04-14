5257d20e394eba3f3f0ee1aeaa7e490a37b106cc
D. Sunshine <david@sunshines.org>
2025-04-14 14:57:17 -0400

    Comments + support for stack

--- flake.nix
+++ flake.nix
@@ -4,36 +4,68 @@
   outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
-      ghcVersion = "98";
+      # If using stack this version needs to match an LTS version from
+      # https://www.stackage.org/
+      ghcVersion = "984";
       pkgs = nixpkgs.legacyPackages.${system};
       hPkgs = pkgs.haskell.packages."ghc${ghcVersion}";
       ghc = hPkgs.ghcWithPackages (ps:
-        with ps; [
+        with ps;
+        [
           # Packages to make available to GHC
           ghcid # Needed for nixvim LSP
-          megaparsec
+          # Other Haskell modules go here:
         ]);
       myLocalDevTools = with pkgs; [
         # System level dependencies
-        ghc
-        cabal-install
-        haskellPackages.implicit-hie # Needed for nixvim LSP
+        ghc # GHC compiler in the desired version (will be available on PATH)
+        cabal-install # Cabal
+        stack-wrapped # See below for what this is
+        haskellPackages.implicit-hie # Needed for nixvim LSP (auto generate LSP hie.yaml file from cabal)
         (haskell-language-server.override {
           supportedGhcVersions = [ "${ghcVersion}" ];
         })
+        # Other development tools:
+        /* haskellPackages.ormolu # Haskell formatter
+           haskellPackages.hlint # Haskell codestyle checker
+           haskellPackages.hoogle # Lookup Haskell documentation
+           haskellPackages.retrie # Haskell refactoring tool
+           haskellPackages.threadscope # A graphical tool for profiling parallel Haskell programs
+           haskellPackages.markdown-unlit # Literate Haskell support for Markdown
+        */
+        # External libraries that may be required:
+        zlib # External C library needed by some Haskell packages
       ];
-      myLibDeps = with pkgs;
-        [
-          # External libraries that may be required
-          zlib
-        ];
+
+      # Wrap Stack to work with our Nix integration. We don't want to modify
+      # stack.yaml so non-Nix users don't notice anything.
+      # - no-nix: We don't want Stack's way of integrating Nix.
+      # --system-ghc    # Use the existing GHC on PATH (will come from this Nix file)
+      # --no-install-ghc  # Don't try to install GHC if no matching GHC found on PATH
+      # https://docs.haskellstack.org/en/stable/topics/nix_integration/
+      stack-wrapped = pkgs.symlinkJoin {
+        name = "stack"; # Make this wrapped version as the usual `stack`
+        paths = [ pkgs.stack ];
+        buildInputs = [ pkgs.makeWrapper ];
+        postBuild = ''
+          wrapProgram $out/bin/stack \
+            --add-flags "\
+              --no-nix \
+              --system-ghc \
+              --no-install-ghc \
+            "
+        '';
+      };
     in {
       defaultPackage.${system} = pkgs.haskellPackages.developPackage {
         root = ./.;
         modifier = drv: pkgs.haskell.lib.addBuildTools drv myLocalDevTools;
       };
       devShells.${system}.default = pkgs.mkShell {
-        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath myLibDeps;
+        # Make external Nix c libraries like zlib known to GHC, like
+        # pkgs.haskell.lib.buildStackProject does
+        # https://github.com/NixOS/nixpkgs/blob/d64780ea0e22b5f61cd6012a456869c702a72f20/pkgs/development/haskell-modules/generic-stack-builder.nix#L38
+        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath myLocalDevTools;
         nativeBuildInputs = myLocalDevTools;
       };
     };
